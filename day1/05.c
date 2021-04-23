516154c980079ae38055f4ad37dc4d42589c3ecb
謝雨 <31348797+XieYuQAQ@users.noreply.github.com>
2021-04-23 15:04:47 +0800

    Update 05.c

--- day1/05.c
+++ day1/05.c
@@ -1,13 +1,14 @@
+
 #include <stdio.h>
 #include <math.h>
 int main()
 {
-    int num,num1,num2;
-    for(num=0;num<=100000;num++)
+    int num,num1,num2,pd;
+    for(num=0;pd!=1;num++)
     {
         num1=sqrt(num+100);
         num2=sqrt(num+268);
-        if(num1*num1==num+100&&num2*num2==num+268)
+        if(pd=(num1*num1==num+100&&num2*num2==num+268))
             {
                 printf("%d\n",num);
                 break;
